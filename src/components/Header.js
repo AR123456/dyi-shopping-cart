44da18af690f6d3a250c81cf0a4aef88d54338ba
AR123456 <coloradoz99@gmail.com>
2022-02-02 07:05:31 -0700

    basic header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <div>
+    <>
+      <header className="block row center">
+        <div>
+          <a href="#/">
+            <h1>DYI Shopping Cart</h1>
+          </a>
+        </div>
+        <div>
+          <a href="#/cart">Cart</a>
+          <a href="#/signin">SignIn</a>
+        </div>
+      </header>
       <h1>this is my header </h1>
-    </div>
+    </>
   );
 };
 
