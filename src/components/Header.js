c3500957ab97c993123d4653903263ec0869d81c
AR123456 <coloradoz99@gmail.com>
2022-02-04 06:22:35 -0700

    header style and input for search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Navbar, Container, FormControl } from "react-bootstrap";
 
 const Header = () => {
   return (
     <div>
-      <h3>This is my header </h3>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        text="white"
+        style={{ height: 80, color: "white" }}
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            {" "}
+            <a href="#">Shopping Cart</a>
+          </Navbar.Brand>
+          <Navbar.Text className="search ">
+            {/* input tag for reactstrap */}
+            <FormControl
+              style={{ width: 500 }}
+              placeholder="Search"
+            ></FormControl>
+          </Navbar.Text>
+        </Container>
+      </Navbar>
     </div>
   );
 };
