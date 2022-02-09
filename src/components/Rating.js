543004c6e56d1199a39df47f26cf137551ee0e68
AR123456 <coloradoz99@gmail.com>
2022-02-09 08:28:56 -0700

    showing start

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 // getting state from parent component
 const Rating = ({ rating, onClick, style }) => {
-  return <div>Rating</div>;
+  //just using the index in the map here
+  return (
+    <div>
+      {[...Array(5)].map((_, i) => (
+        //with each itteration check rating if greater than index, if greater than index AiFillStart
+        <span>
+          {rating > i ? (
+            <AiFillStar fontSize="15px" />
+          ) : (
+            <AiOutlineStar fontSize="15ps" />
+          )}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Rating;
+// 32:31 resume  https://www.youtube.com/watch?v=HptuMAUaNGk
