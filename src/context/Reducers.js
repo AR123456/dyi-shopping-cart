e666f3077150deb9a6c5694bcd0db6329ce053e8
AR123456 <coloradoz99@gmail.com>
2022-02-10 05:47:50 -0700

    add and remove reducer

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -1,8 +1,24 @@
 export const cartReducer = (state, action) => {
   // setting up on action for which to run the switch case
-  //   switch (action.type) {
-  //     default:
-  //       return state;
-  //   }
-  return state;
+  // functionality to add and remove from cart
+
+  switch (action.type) {
+    case "ADD_TO_CART":
+      // state and manipulated cart state
+      // need type and payload
+      // payload is what we are adding to the cart
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, qty: 1 }],
+      };
+    case "REMOVE_FROM_CART":
+      // filter out the removed product
+      // filter out if action.payload is not add to cart
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
 };
