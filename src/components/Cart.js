793d1191ea48be5cff0a9e00ffd78e54702f920d
AR123456 <coloradoz99@gmail.com>
2022-02-11 04:33:30 -0700

    start cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
+// get cart state from context
+import { CartState } from "../context/Context";
 
 const Cart = () => {
-  return <div> Cart</div>;
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+  return (
+    <div className="home">
+      {/* render to products page  */}
+      <div className="productContainer">
+        {/* react bootsrap list group */}
+        <ListGroup>{/* map the cart  */}</ListGroup>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
+// resume at https://www.youtube.com/watch?v=HptuMAUaNGk
