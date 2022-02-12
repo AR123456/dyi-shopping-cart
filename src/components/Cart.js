7012d71938f4d461b313fa2f44106b46ecb90ae9
AR123456 <coloradoz99@gmail.com>
2022-02-12 09:09:05 -0700

    totaling cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ListGroup } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, ListGroup } from "react-bootstrap";
 // get cart state from context
 import { CartState } from "../context/Context";
 
@@ -8,12 +8,36 @@ const Cart = () => {
     state: { cart },
     dispatch,
   } = CartState();
+  //  local state to calculate total
+  const [total, setTotal] = useState();
+  // reduce the cart var use cart.price to calc total price
+  useEffect(() => {
+    // ruduce takes an accumulator and current
+    // curr.price is a string to make it a number
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+    );
+    // run every time cart var changes
+  }, [cart]);
   return (
     <div className="home">
       {/* render to products page  */}
       <div className="productContainer">
         {/* react bootsrap list group */}
-        <ListGroup>{/* map the cart  */}</ListGroup>
+        <ListGroup>
+          {/* map the cart  */}
+          {cart.map((prod) => (
+            <span>{prod.name}</span>
+          ))}
+        </ListGroup>
+      </div>
+      <div className="filters summary">
+        <span className="title">Subtotal {cart.length} items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>Total: $ {total}</span>
+        <Button type="button" disabled={cart.length === 0}>
+          {" "}
+          Proceed to Checkout
+        </Button>
       </div>
     </div>
   );
