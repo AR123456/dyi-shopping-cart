5dddd4ef0c97fb3d1f7f0786a2377d1bf46e1ab9
AR123456 <coloradoz99@gmail.com>
2022-02-12 09:09:50 -0700

    styling cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import Rating from "../components/Rating.js";
+import { Button, Col, Form, Image, ListGroup, Row } from "react-bootstrap";
 // get cart state from context
 import { CartState } from "../context/Context";
 
@@ -27,7 +28,29 @@ const Cart = () => {
         <ListGroup>
           {/* map the cart  */}
           {cart.map((prod) => (
-            <span>{prod.name}</span>
+            <ListGroup.Item key={prod.id}>
+              <Row>
+                <Col md={2}>
+                  <Image src={prod.image} alt={prod.name} fluid rouned></Image>
+                </Col>
+                <Col md={2}>
+                  <span>{prod.name}</span>
+                </Col>
+                <Col md={2}>
+                  <span>$ {prod.price}</span>
+                </Col>
+                <Col md={2}>
+                  <Rating rating={prod.ratings}></Rating>
+                </Col>
+                <Col md={2}>
+                  <Form.Control as="select" value={prod.quty}>
+                    {[...Array(prod.inStock).keys()].map((x) => (
+                      <option key={x + 1}>{x + 1}</option>
+                    ))}
+                  </Form.Control>
+                </Col>
+              </Row>
+            </ListGroup.Item>
           ))}
         </ListGroup>
       </div>
