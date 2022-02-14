cf08de73fe74f8c8d3e90772e87f7b163c7a5899
AR123456 <coloradoz99@gmail.com>
2022-02-14 07:30:51 -0700

    sort asending borken

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,37 @@ const Home = () => {
   // getting products from context
   const {
     state: { products },
+    // bring in productState
+    productState: { sort, byStock, byFastDelivery, byRating, serachQuery },
   } = CartState();
   // console.log(products);
+  // this function will manipulate the state and show filtered products
+  const transformProducts = () => {
+    let sortedProducts = products;
+    if (sort) {
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+      //The sort() method sorts the elements of an array in place and returns the sorted array. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+    return sortedProducts;
+  };
+
   return (
     <div className="home">
       <Filters></Filters>
-      {/* map through products  */}
-      <div className="productContainer">
+      {/* map through products  before filter implemented  */}
+      {/* <div className="productContainer">
         {products.map((prod) => {
           return <SingleProduct prod={prod} key={prod.id}></SingleProduct>;
         })}
+      </div> */}
+      {/* rendering the products with filtering implemented  */}
+      <div className="productContainer">
+        {transformProducts().map((prod) => (
+          <SingleProduct prod={prod} key={prod.id} />
+        ))}
       </div>
     </div>
   );
