c96b434d9de62692e98ebc517108289e708bdfb3
AR123456 <coloradoz99@gmail.com>
2022-03-28 07:22:28 -0600

    move to local data file

--- frontend/src/context/Context.js
+++ frontend/src/context/Context.js
@@ -1,24 +1,25 @@
 import React, { createContext, useContext, useReducer } from "react";
-import faker from "faker";
+// import faker from "faker";
 import { cartReducer, productReducer } from "./Reducers";
-// import products from "../products";
+import products from "../products";
 // console.log(products);
 const Cart = createContext();
 // this so faker only renders one time
-faker.seed(99);
+// faker.seed(99);
 // context will wrap create react app
 // children are coming from index.js
 const Context = ({ children }) => {
   // using faker to get an array products
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
-    name: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
-    fastDelivery: faker.datatype.boolean(),
-    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-  }));
+  // const products = [...Array(20)].map(() => ({
+  //   id: faker.datatype.uuid(),
+  //   name: faker.commerce.productName(),
+  //   price: faker.commerce.price(),
+  //   image: faker.random.image(),
+  //   inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+  //   fastDelivery: faker.datatype.boolean(),
+  //   ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  // }));
+
   console.log(products);
   // userReducer hook - the reducer is a switch case
   // const [state, dispatch] = useReducer(reducer, initialState);
