96f31693b041f211ccee5ffc9fb234889ffa4f1a
AR123456 <coloradoz99@gmail.com>
2022-03-29 06:15:16 -0600

    dotenv setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
-// const of products call from faker on the back end now
+// env
+const dotenv = require("dotenv");
+// get dummy products from the file
+// const products = require("./data/products");
+// bring in dotenv
+dotenv.config();
 
 // init express app
 const app = express();
-const port = 5000;
+
 // home route
 app.get("/", (req, res) => {
   res.send("API is running.....");
@@ -14,8 +18,16 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p.id === req.params.id);
+  const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+// putting prot into const
+const PORT = process.env.PORT || 5000;
+
 // kick off server
-app.listen(5000, console.log(`Server is running on http://localhost:${port}`));
+app.listen(
+  5000,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+  )
+);
